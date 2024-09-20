0f221a098593d1f75cbd8b520c2fe5d5127a6443
imgrid <iachamorro.5365@unicesmag.edu.co>
2024-09-20 12:16:33 -0500

    Endpoint: Database connection

---
+++ api/config/db_connection.php
@@ -0,0 +1,29 @@
+<?php
+/* Postgres Database connection
+Developer:ingrid
+*/
+
+   $host = "localhost"; //127.0.0.1
+   $username   = "postgres";
+   $password   = "unicesmag";
+   $dbname     = "beta";
+   $port       = "5432";
+
+  $data_connection = "
+     host     = $host
+     port     = $port
+     dbname   = $dbname
+     user     = $username
+     password = $password
+    ";
+
+   $conn =pg_connect($data_connection);
+
+   if (!$conn) {
+      die("Connection failed: ". pg_last_error());
+   }else{
+      echo "Connected successfully";
+   }
+
+   pg_close($conn);
+?>
