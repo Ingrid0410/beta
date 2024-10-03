f49198b6b5ca50785c4fa94dfd5065b2b5bccb5a
imgrid <iachamorro.5365@unicesmag.edu.co>
2024-10-03 12:04:13 -0500

    create table

--- api/config/db_connection.php
+++ api/config/db_connection.php
@@ -1,29 +1,34 @@
 <?php
-/* Postgres Database connection
-Developer:ingrid
-*/
+     /*
+    Postgres Database connection
+    development: Jorge
+    */
 
-   $host = "localhost"; //127.0.0.1
-   $username   = "postgres";
-   $password   = "unicesmag";
-   $dbname     = "beta";
-   $port       = "5432";
+    //entrar en el navegador 127.0.0.1/beta/api/config/db_conncetion.php 
 
-  $data_connection = "
-     host     = $host
-     port     = $port
-     dbname   = $dbname
-     user     = $username
-     password = $password
+    //Creacion de variables
+    $host = "localhost"; //127.0.0.1
+    $username    = "postgres";
+    $password   = "unicesmag";
+    $dbname     = "beta";
+    $port       = "5432";
+
+    $data_connectionn = "
+       host     = $host
+       port     = $port
+       dbname   = $dbname
+       user     = $username
+       password = $password
     ";
 
-   $conn =pg_connect($data_connection);
+    $conn = pg_connect($data_connectionn);
 
-   if (!$conn) {
-      die("Connection failed: ". pg_last_error());
-   }else{
-      echo "Connected successfully";
-   }
+    if (!$conn) {
+        die("Connection failed: ". pg_last_error());
+    }else{
+        //echo "Connected successfully";
+    }
 
-   pg_close($conn);
+    //pg_close($conn);
+
 ?>
